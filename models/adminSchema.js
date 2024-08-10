0266dd29014c291db6d598f5ca1545aff3eecd4a
Nadeem Ahmad <145861674+nadeemahmed6308@users.noreply.github.com>
2024-08-10 07:04:01 +0500

    Add files via upload

---
+++ models/adminSchema.js
@@ -0,0 +1,28 @@
+import mongoose from 'mongoose';
+
+const adminSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+    email: {
+        type: String,
+        unique: true,
+        required: true,
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+    role: {
+        type: String,
+        default: "Admin"
+    },
+    schoolName: {
+        type: String,
+        unique: true,
+        required: true
+    }
+});
+
+export default mongoose.model('Admin', adminSchema);
